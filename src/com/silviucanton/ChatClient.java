0594b813ce6604d8fe55704ec38134f9bfb82f14
Silviu Anton <silviu.c.anton@gmail.com>
2019-01-15 13:58:01 +0200

    Implementare recieve - mesaje
    Am implementat functionalitatea clientului de a astepta si primi mesaje, in timp ce GUI-ul este inca utilizabil, utilizand un thread secundar

--- src/com/silviucanton/ChatClient.java
+++ src/com/silviucanton/ChatClient.java
@@ -3,25 +3,41 @@ package com.silviucanton;
 import java.io.*;
 import java.net.*;
 import javax.swing.*;
+import java.util.*;
 import java.awt.*;
 import java.awt.event.*;
 
 public class ChatClient {
 	
+	JTextArea incoming;
 	JTextField outgoing;
+	BufferedReader reader;
 	PrintWriter writer;
 	Socket sock;
 	
 	public void go() {
 		JFrame frame = new JFrame("Chat Client");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JPanel mainPanel = new JPanel();
+		incoming = new JTextArea(15, 50);
+		incoming.setLineWrap(true);
+		incoming.setWrapStyleWord(true);
+		incoming.setEditable(false);
+		JScrollPane qscroller = new JScrollPane(incoming);
+		qscroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		qscroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		outgoing = new JTextField(20);
 		JButton sendButton = new JButton("Send");
 		sendButton.addActionListener(new MySendActionListener());
+		mainPanel.add(qscroller);
 		mainPanel.add(outgoing);
 		mainPanel.add(sendButton);
-		frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
 		setUpNetworking();
+		
+		Thread readerThread = new Thread(new IncomingReader());
+		readerThread.start();
+		
+		frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
 		frame.setSize(400, 500);
 		frame.setVisible(true);
 	}
@@ -46,6 +62,21 @@ public class ChatClient {
 			}
 			outgoing.setText("");
 			outgoing.requestFocus();
+		}
+	}
+	
+	public class IncomingReader implements Runnable {
+		public void run() {
+			String message;
+			try {
+				
+				while((message = reader.readLine()) != null) {
+					System.out.println("read" + message);
+					incoming.append(message + "\n");
+				}
+			} catch(Exception ex) {
+				ex.printStackTrace();
+			}
 		}
 	}
 	
